7aa6e33e63e05eabca57d149a67df2dceea01800
ladesign <jeywang@gmail.com>
2025-08-25 15:58:38 +0800

    10 使用Iles提供的Svelte元件創建TheFooter頁尾元件

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -10,12 +10,13 @@
   })
 </script>
 <template>
-  <div>
-    <div class="flex flex-col bg-white dark:bg-neutral-950 text-neutral-900 dark:text-neutral-50">
-      <main>
-        <slot />
-      </main>
+    <div>
+        <div class="flex flex-col bg-white dark:bg-neutral-950 text-neutral-900 dark:text-neutral-50">
+            <div class="shadow">
+                <slot/>
+            </div>
+        </div>
+        <TheFooter client:none></TheFooter>
     </div>
-  </div>
 </template>
 <style></style>
